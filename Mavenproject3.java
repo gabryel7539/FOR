e72c7afa4b34d09b2d11afa62f676e4f7330a18b
gabryel7539 <pgabryel144@gmail.com>
2024-12-06 20:42:24 -0300

    Add files via upload

---
+++ Mavenproject3.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject3;
+
+/**
+ *
+ * @author Admin
+ */
+public class Mavenproject3 {
+    
+    
+    public static void main(String[] args) {
+        
+        int soma=0;
+        
+     for( int i=0; i<=50 ; i++ ){
+         
+          soma += i;      
+           }
+          
+          System.out.println("A soma de 1 ate 50 é " +soma);
+}
+}
