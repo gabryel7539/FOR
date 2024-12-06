b9c5e9a0194e1d056e3de7eaae1628142230dd97
gabryel7539 <pgabryel144@gmail.com>
2024-12-06 20:42:51 -0300

    Add files via upload

---
+++ FOR2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.for2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class FOR2 {
+
+     public static void main(String[] args) {
+      
+       Scanner scanner = new Scanner (System.in);
+        
+   
+       System.out.print("Digite um numero: ");
+        int nu= scanner.nextInt();
+        
+        
+        for ( int i=1; i<=11 ; i++ ){
+            System.out.println(i+"x"+nu+"="+(nu*i));
+     
+       scanner.close();
+    
+        }   
+    }
+}
