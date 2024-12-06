689b23ddaa4e57453312abca9d74b76be7aabf28
gabryel7539 <pgabryel144@gmail.com>
2024-12-06 19:20:26 -0300

    Add files via upload

---
+++ FOR2/src/main/java/com/mycompany/for2/FOR2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.for2;
+
+/**
+ *
+ * @author Admin
+ */
+public class FOR2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
